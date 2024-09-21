1dfb21f3135e4b51f97217a79ab1acea93175066
whysamay <samaypawar2200@gmail.com>
2024-09-21 19:20:10 +0530

    this is my third commit, exception,logger.py etc

--- src/exception.py
+++ src/exception.py
@@ -22,9 +22,3 @@ class CustomException(Exception):
         return self.error_message
 
 
-if __name__=="__main__":
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("exception divide by zero")
-        raise CustomException(e,sys)
--- src/logger.py
+++ src/logger.py
@@ -15,7 +15,6 @@ logging.basicConfig(
 
 )
 
-if __name__=="__main__":
-    logging.info("Logging has started")
+
 
 ##tocheck if it is workinh
